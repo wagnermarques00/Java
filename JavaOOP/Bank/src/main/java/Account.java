e82703084d006a8babb4c47c13ee4e9ad1bab334
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-09-05 18:21:34 -0300

    Java OO 03/07

--- JavaOOP/Bank/src/main/java/Account.java
+++ JavaOOP/Bank/src/main/java/Account.java
@@ -3,4 +3,25 @@ public class Account {
     int agency;
     int number;
     String ownerName;
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if(this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfer(double amount, Account destinationAccount) {
+        if(this.balance >= amount) {
+            this.balance -= amount;
+            destinationAccount.deposit(amount);
+            return true;
+        }
+        return false;
+    }
 }
---
+++ JavaOOP/Bank/src/main/java/TestMethods.java
@@ -0,0 +1,29 @@
+public class TestMethods {
+    public static void main(String[] args) {
+        Account account = new Account();
+        Account secondAccount = new Account();
+
+        account.balance = 100;
+        System.out.println("Account balance: " + account.balance);
+
+        account.deposit(50);
+        System.out.println("Account balance after deposit: " + account.balance);
+
+        boolean withdrawSucceeded = account.withdraw(25);
+        System.out.println("Account withdraw successful: " + withdrawSucceeded);
+        System.out.println("Account balance after withdraw: " + account.balance);
+
+        secondAccount.deposit(1000);
+        System.out.println("Second account balance:" + secondAccount.balance);
+
+        boolean transferSucceeded = secondAccount.transfer(300, account);
+        // Change the first argument to a value greater than the balance to return the second condition
+        if (transferSucceeded) {
+            System.out.println("Second account transfer successful: " + transferSucceeded);
+            System.out.println("First account balance after transfer: " + account.balance);
+            System.out.println("Second account balance after transfer: " + secondAccount.balance);
+        } else {
+            System.out.println("Second account transfer failed");
+        }
+    }
+}
