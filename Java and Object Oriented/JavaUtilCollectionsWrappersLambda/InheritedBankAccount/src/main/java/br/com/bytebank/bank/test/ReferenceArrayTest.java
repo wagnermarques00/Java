fa4cde9913e2a7a44782258e3647632b6bd03f0d
Wagner Marques Oliveira <wagnermarques00@gmail.com>
2022-09-23 18:34:03 -0300

    Util 02/08

--- Java and Object Oriented/JavaUtilCollectionsWrappersLambda/InheritedBankAccount/src/main/java/br/com/bytebank/bank/test/ReferenceArrayTest.java
+++ Java and Object Oriented/JavaUtilCollectionsWrappersLambda/InheritedBankAccount/src/main/java/br/com/bytebank/bank/test/ReferenceArrayTest.java
@@ -1,6 +1,8 @@
 package br.com.bytebank.bank.test;
 
+import br.com.bytebank.bank.model.Account;
 import br.com.bytebank.bank.model.CheckingAccount;
+import br.com.bytebank.bank.model.SavingsAccount;
 
 public class ReferenceArrayTest {
 
@@ -9,19 +11,20 @@ public class ReferenceArrayTest {
 		CheckingAccount ca2 = new CheckingAccount(222, 333);
 		CheckingAccount ca3 = new CheckingAccount(333, 444);
 		CheckingAccount ca4 = new CheckingAccount(444, 555);
-		CheckingAccount ca5 = new CheckingAccount(555, 666);
+		SavingsAccount ca5 = new SavingsAccount(555, 666);
 
-		CheckingAccount[] checkingAccounts = { ca1, ca2, ca3, ca4, ca5 };
+		Object[] references = { ca1, ca2, ca3, ca4, ca5 };
 
-		System.out.println(checkingAccounts[0]); // Checking Account - Agency: 111, Number: 222
-		System.out.println(checkingAccounts[1]); // Checking Account - Agency: 222, Number: 333
-		System.out.println(checkingAccounts[2]); // Checking Account - Agency: 333, Number: 444
-		System.out.println(checkingAccounts[3]); // Checking Account - Agency: 444, Number: 555
-		System.out.println(checkingAccounts[4]); // Checking Account - Agency: 555, Number: 666
+		System.out.println(references[0]); // Checking Account - Agency: 111, Number: 222
+		System.out.println(references[1]); // Checking Account - Agency: 222, Number: 333
+		System.out.println(references[2]); // Checking Account - Agency: 333, Number: 444
+		System.out.println(references[3]); // Checking Account - Agency: 444, Number: 555
+		System.out.println(references[4]); // Savings Account - Agency: 555, Number: 666
 
-		System.out.println(checkingAccounts[0].getNumber()); // 222
+		CheckingAccount referenced = (CheckingAccount) references[0];
+		System.out.println(referenced.getNumber()); // 222
 
-		CheckingAccount ref = checkingAccounts[2];
+		Account ref = (Account) references[2];
 		System.out.println(ref.getAgency()); // 333
 	}
 
