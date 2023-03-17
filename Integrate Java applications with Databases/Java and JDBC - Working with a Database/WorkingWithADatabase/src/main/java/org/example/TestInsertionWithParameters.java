2e54b7bb1741cd2e9bf12d0efebdd54228ea63cf
wagnermarques00 <wagnermarques00@gmail.com>
2023-03-17 17:11:58 -0300

    JDBC 04/08

--- Integrate Java applications with Databases/Java and JDBC - Working with a Database/WorkingWithADatabase/src/main/java/org/example/TestInsertionWithParameters.java
+++ Integrate Java applications with Databases/Java and JDBC - Working with a Database/WorkingWithADatabase/src/main/java/org/example/TestInsertionWithParameters.java
@@ -9,25 +9,42 @@ import java.sql.Statement;
 public class TestInsertionWithParameters {
 
 	public static void main(String[] args) throws SQLException {
-		String name = "Mouse'"; // Simulating a possible error without treatment
-		String description = "Wireless Mouse";
 		String insertProduct = "INSERT INTO product(name, description) VALUES(?, ?)";
 
 		ConnectionFactory connectionFactory = new ConnectionFactory();
 		Connection connection = connectionFactory.recoverConnection();
+		connection.setAutoCommit(false);
 
-		PreparedStatement statement = connection.prepareStatement(insertProduct, Statement.RETURN_GENERATED_KEYS);
+		try(connection) {
+			PreparedStatement statement = connection.prepareStatement(insertProduct, Statement.RETURN_GENERATED_KEYS);
+
+			try(statement) {
+				addVariable("SmartTv", "45 inches television", statement);
+				addVariable("Smartphone", "Smartphone Samsung", statement);
+
+				connection.commit();
+			} catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("Rollback executed");
+
+				connection.rollback();
+			}
+		}
+	}
+
+	private static void addVariable(String name, String description, PreparedStatement statement) throws SQLException {
 		statement.setString(1, name);
 		statement.setString(2, description);
 
 		statement.execute();
 
 		ResultSet resultSet = statement.getGeneratedKeys();
-		while(resultSet.next()) {
-			Integer id = resultSet.getInt(1);
-			System.out.println("The id created in the product table was: " + id);
-		}
 
-		connection.close();
+		try(resultSet) {
+			while(resultSet.next()) {
+				int id = resultSet.getInt(1);
+				System.out.println("The id created in the product table was: " + id);
+			}
+		}
 	}
 }
